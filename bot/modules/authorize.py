c3b21c643d2c8bd51a65677d86ba882aad61b534
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2021-10-14 14:54:50 +0530

    Modules: Authorize

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -16,18 +16,18 @@ def authorize(update,context):
             if chat_id not in AUTHORIZED_CHATS:
                 file.write(f'{chat_id}\n')
                 AUTHORIZED_CHATS.add(chat_id)
-                msg = 'Chat authorized'
+                msg = '☑️Chat authorized'
             else:
-                msg = 'Already authorized chat'
+                msg = '☑️Already authorized Chat'
         else:
             # Trying to authorize someone in specific
             user_id = reply_message.from_user.id
             if user_id not in AUTHORIZED_CHATS:
                 file.write(f'{user_id}\n')
                 AUTHORIZED_CHATS.add(user_id)
-                msg = 'Person Authorized to use the bot!'
+                msg = '✅User Authorized to use the bot!'
             else:
-                msg = 'Person already authorized'
+                msg = '✅Already authorized User'
         sendMessage(msg, context.bot, update)
 
 
@@ -38,17 +38,17 @@ def unauthorize(update,context):
         chat_id = update.effective_chat.id
         if chat_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(chat_id)
-            msg = 'Chat unauthorized'
+            msg = '⚠️Chat unauthorized!'
         else:
-            msg = 'Already unauthorized chat'
+            msg = '⚠️Already unauthorized Chat!'
     else:
         # Trying to authorize someone in specific
         user_id = reply_message.from_user.id
         if user_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(user_id)
-            msg = 'Person unauthorized to use the bot!'
+            msg = '⚠️User unauthorized to use the bot!'
         else:
-            msg = 'Person already unauthorized!'
+            msg = '⚠️Already unauthorized User!'
     with open('authorized_chats.txt', 'a') as file:
         file.truncate(0)
         for i in AUTHORIZED_CHATS:
