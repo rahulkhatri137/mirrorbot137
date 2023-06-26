f236ccf6b0f97eac7b1c9afd44920f81c446ba90
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2023-06-26 12:13:59 +0530

    filter main

--- bot/__main__.py
+++ bot/__main__.py
@@ -35,7 +35,6 @@ from bot.modules import (  # noqa
     count,
 )
 
-
 def stats(update, context):
     currentTime = get_readable_time(time.time() - botStartTime)
     total, used, free = shutil.disk_usage(".")
@@ -59,17 +58,15 @@ def stats(update, context):
             f"<b>├  📥 Total Download : {recv}</b>\n"
             f"<b>├  🖥️ CPU : {cpuUsage}%</b>\n"
             f"<b>├  🚀 RAM : {memory}%</b>\n"
-            f"<b>└  🗄 DISK : {disk}%</b>\n\n"
-            f"<b>【 🌟 Powered by @Bots137 】</b>"
+            f"<b>└  🗄 DISK : {disk}%</b>"
     )
     sendMessage(stats, context.bot, update)
 
 def start(update, context):
-    if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
+    if CustomFilters.authorized(update):
         start_string = f'''
 This bot is designed by @rahulkhatri137 to mirror your links to Google Drive and upload files to Telegram!
-Type /{BotCommands.HelpCommand} to get a list of available commands.
-Powered by @Bots137
+Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMessage(start_string, context.bot, update)
     else:
@@ -88,51 +85,36 @@ def restart(update, context):
     fs_utils.clean_all()
     os.execl(executable, executable, "-m", "bot")
 
-
 def ping(update, context):
     start_time = int(round(time.time() * 1000))
     reply = sendMessage("Starting Ping", context.bot, update)
     end_time = int(round(time.time() * 1000))
     editMessage(f"{end_time - start_time} ms", reply)
 
-
 def log(update, context):
     sendLogFile(context.bot, update)
-
 
 def bot_help(update, context):
     help_string = f"""
 /{BotCommands.HelpCommand}: To get this message
-
 /{BotCommands.MirrorCommand} [download_url][magnet_link]: Start mirroring the link to google drive.
-
 /{BotCommands.CloneCommand} [Drive_Link]: Copy link to google drive
 /{BotCommands.CountCommand} [Drive_Link]: Count Files Of a Drive Link
-
 /{BotCommands.UnzipMirrorCommand} [download_url][magnet_link] : starts mirroring and if downloaded file is any archive , extracts it to google drive
 /{BotCommands.ZipMirrorCommand} [download_url][magnet_link]: start mirroring and upload the archived (.zip) version of the download
-
 /{BotCommands.LeechCommand} [download_url][magnet_link]: Start mirroring the link to telegram
 /{BotCommands.UnzipLeechCommand} [download_url][magnet_link] : starts mirroring and if downloaded file is any archive , extracts it to telegram
 /{BotCommands.ZipLeechCommand} [download_url][magnet_link]: start mirroring and upload the archived (.zip) version of the download
-
 /{BotCommands.WatchCommand} [youtube-dl supported link]: Mirror through youtube-dl. Click /{BotCommands.WatchCommand} for more help
 /{BotCommands.LeechWatchCommand} Mirror Youtube-dl support link(Leech)"),
 /{BotCommands.LeechZipWatchCommand} Mirror Youtube playlist link as .zip(Leech)"),
 /{BotCommands.ZipWatchCommand} [youtube-dl supported link]: Mirror through youtube-dl and zip before uploading
-
 /{BotCommands.CancelMirror} Reply to the message by which the download was initiated and that download will be cancelled
-
 /{BotCommands.StatusCommand} Shows a status of all the downloads
-
 /{BotCommands.SpeedCommand} Test Internet Speed Of the Bot
-
 /{BotCommands.ListCommand} [search term]: Searches the search term in the Google drive, if found replies with the link
-
 /{BotCommands.StatsCommand} Show Stats of the machine the bot is hosted on
-
 /{BotCommands.AuthorizeCommand} Authorize a chat or a user to use the bot (Can only be invoked by owner of the bot)
-
 /{BotCommands.LogCommand} Get a log file of the bot. Handy for getting crash reports
 /{BotCommands.LeechSetCommand} Leech settings
 /{BotCommands.SetThumbCommand} Reply photo to set it as Thumbnail
@@ -177,29 +159,29 @@ def main():
     ping_handler = CommandHandler(
         BotCommands.PingCommand,
         ping,
-        filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
+        filters=CustomFilters.authorized,
         run_async=True,
     )
     restart_handler = CommandHandler(
         BotCommands.RestartCommand,
         restart,
-        filters=CustomFilters.owner_filter | CustomFilters.sudo_user,
+        filters=CustomFilters.sudo,
         run_async=True,
     )
     help_handler = CommandHandler(
         BotCommands.HelpCommand,
         bot_help,
-        filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
+        filters=CustomFilters.authorized,
         run_async=True,
     )
     stats_handler = CommandHandler(
         BotCommands.StatsCommand,
         stats,
-        filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
+        filters=CustomFilters.authorized,
         run_async=True,
     )
     log_handler = CommandHandler(
-        BotCommands.LogCommand, log, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True
+        BotCommands.LogCommand, log, filters=CustomFilters.sudo, run_async=True
     )
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(ping_handler)
