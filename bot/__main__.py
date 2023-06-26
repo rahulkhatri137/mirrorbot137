a9ad0b6932b3e2b7a8856b17dbdff14ed37cc5f9
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2023-06-26 12:23:51 +0530

    Powered by Bots137

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,7 +58,8 @@ def stats(update, context):
             f"<b>├  📥 Total Download : {recv}</b>\n"
             f"<b>├  🖥️ CPU : {cpuUsage}%</b>\n"
             f"<b>├  🚀 RAM : {memory}%</b>\n"
-            f"<b>└  🗄 DISK : {disk}%</b>"
+            f"<b>└  🗄 DISK : {disk}%</b>\n\n"
+            f"<b>【 🌟 Powered by @Bots137 】</b>"
     )
     sendMessage(stats, context.bot, update)
 
@@ -66,7 +67,8 @@ def start(update, context):
     if CustomFilters.authorized(update):
         start_string = f'''
 This bot is designed by @rahulkhatri137 to mirror your links to Google Drive and upload files to Telegram!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+Type /{BotCommands.HelpCommand} to get a list of available commands.
+Powered by @Bots137
 '''
         sendMessage(start_string, context.bot, update)
     else:
